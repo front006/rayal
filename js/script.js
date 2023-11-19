6af7b1ad6380c0862cde2b54e84775762b328181
@front006 <124555788+front006@users.noreply.github.com>
2023-11-19 15:55:11 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const burger = document.querySelector('.burger')
+const menu = document.querySelector('.header__nav')
+
+burger.addEventListener('click', function () {
+	menu.classList.toggle('header__nav--active')
+	burger.classList.toggle('burger--active')
+})
